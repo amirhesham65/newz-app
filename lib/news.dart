4274dd7ff18074beed0468b479d9a50273212d09
amirhesham65 <amirhesham65@gmail.com>
2019-09-29 20:50:10 +0200

    Added more comments

--- lib/news.dart
+++ lib/news.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 
+// A single News post definition
 class NewsPost {
   String title;
   String description;
@@ -8,12 +9,14 @@ class NewsPost {
   NewsPost({ this.title, this.description});  
 }
 
+// A news topic definition
 class Topic {
   String topic;
   List<NewsPost> news;
 
   Topic({ this.topic });
 
+  // Getting all the news related to the the topic
   Future<void> getNews() async {
     Response response = await get('https://newsapi.org/v2/everything?q=$topic&apiKey=d3442b53f9d24711878e06f62a121f78');
     Map<String, dynamic> data = jsonDecode(response.body);
